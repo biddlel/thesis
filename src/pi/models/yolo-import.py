9e195230ffa765f803bb36f9cf09fdea6abe5ae6
Liam.pi <biddleliam@outlook.com>
2025-07-30 11:14:13 +1000

    added model import

--- src/pi/models/yolo-import.py
+++ src/pi/models/yolo-import.py
@@ -3,5 +3,5 @@ from ultralytics import YOLO
 # Load the YOLO11 model
 model = YOLO("yolo11n.pt")
 
-# Export the model to NCNN format
+# Export the model to ONNX format
 model.export(format="onnx")  # creates '/yolo11n_ncnn_model'
